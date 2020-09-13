6b920709e9762df2a320bf0bc347aced23d3d95a
Rama Seftiansyah <ramaseftiansyah12@gmail.com>
2020-09-13 19:04:39 +0700

    Migrasi Pure HTML & CSS ke React

--- src/App.js
+++ src/App.js
@@ -3,9 +3,60 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-btn">Add</button>
+          <h1 className="header-title">ToDo List</h1>
+          <button className="header-btn main-red-color">Clear</button>
+        </section>
+
+        <section className="add">
+          <form className="add-form">
+            <input type="text" className="add-input" />
+            <button className="add-btn">Submit</button>
+          </form>
+        </section>
+
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">testing1</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
